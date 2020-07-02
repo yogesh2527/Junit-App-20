5175ae480d0f1a048a17c78577f932434a0ffc1b
yogesh bagade <yogeshbagade25@gmail.com>
2020-07-02 12:23:53 +0530

    His-300 related changes

--- src/main/java/com/myproject/UnitTestingApplication.java
+++ src/main/java/com/myproject/UnitTestingApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnitTestingApplication {
 
+	private static final String REST_URL ="www.google.com"
+
 	public static void main(String[] args) {
 		SpringApplication.run(UnitTestingApplication.class, args);
 	}
