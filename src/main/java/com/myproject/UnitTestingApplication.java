f34601bc9c2a36b7b3341fcde7aa00070d83cf63
yogesh bagade <yogeshbagade25@gmail.com>
2020-07-02 15:14:02 +0530

    HIS-301 related changes

--- src/main/java/com/myproject/UnitTestingApplication.java
+++ src/main/java/com/myproject/UnitTestingApplication.java
@@ -6,8 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnitTestingApplication {
 
+	private Map<String , Object > cache = new HashMap<String , Object>();
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UnitTestingApplication.class, args);
 	}
+// HIS-301 changes
+public void loadDataCache()
+	{
+		// logic
+	}
+
 
 }
